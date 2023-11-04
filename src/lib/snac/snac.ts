1b3c87db4576bc20d272817cbc433a8b581330c3
Justin Kelly <justinkelly@justinkelly.net>
2023-11-04 13:48:18 +0100

    more snac

--- src/lib/snac/snac.ts
+++ src/lib/snac/snac.ts
@@ -2,8 +2,8 @@ import xml2snac from "./xml2snac"
 import { SNACItem, SNACElement, SNACText, SNACCDATA, SNACComment, SNACPINode } from "./types"
 
 const find = (snac: SNACItem[], path: number[]): SNACItem | null => {
-    const element: SNACItem = {N:"", A:{}, C: snac, a:false, o:false, q:false}
-    return _find(element , path)
+    const element: SNACItem = { N: "", A: {}, C: snac, a: false, o: false, q: false }
+    return _find(element, path)
 }
 
 const _find = (snac: SNACItem, path: number[]): SNACItem | null => {
@@ -13,7 +13,7 @@ const _find = (snac: SNACItem, path: number[]): SNACItem | null => {
     else {
         if (snac.hasOwnProperty('C')) {
             const S = snac as SNACElement
-            const [i, ...p] = path;
+            const [i, ...p] = path
             if (S.C.length > i) {
                 return _find(S.C[i], p)
             }
@@ -53,4 +53,45 @@ export const findElement = (snac: SNACItem[], path: number[]) => {
     else {
         console.log("No element found")
     }
+}
+
+const getRemovePaths = (removeFrom: number[], removeTo: number[] ) => {
+    if(removeFrom.length !== removeTo.length) {
+        return removeTo
+    }
+    else {
+        for(let i in removeFrom.slice(0, removeFrom.length - 1)) {
+            if(removeFrom[i] !== removeTo[i]) {
+                return removeTo
+            }
+        }
+        const from = removeFrom[removeFrom.length - 1]
+        const to = removeTo[removeTo.length - 1]
+
+        let out:number[] = []
+        if(from > to){
+            for(let i = to; i<=from; i++){
+                out = [...out, i]
+            }
+        }
+        else if(from < to) {
+
+        }
+        else {
+
+        }
+    }
+}
+
+const clone = (snac: SNACItem[], removeFrom: number[], removeTo: number[], replace: SNACItem[] | null): SNACItem[] => {
+    const snac1: SNACItem[] = []
+    for (const s in snac) {
+        if (removeFrom[0] === parseInt(s)) {
+
+        }
+        else {
+            snac1.push(snac[s])
+        }
+    }
+    return snac1
 }
