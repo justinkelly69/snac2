d40476f6b19365eacca4c0545aee92fa9ceb1aca
Justin Kelly <justinkelly@justinkelly.net>
2023-11-05 12:25:15 +0100

    experimenting

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 import xmlInput from './lib/data/xml/waffle'
 import xml2snac from './lib/snac/xml2snac'
-import snac2xml from './lib/snac/snac2xml';
+//import snac2xml from './lib/snac/snac2xml';
 import xmlOut from './lib/tsx/snac2xml';
 import { Attribute, Attributes, CDATA, CloseTag, Text, Comment, OpenTag, PI, Prefix } from './lib/tsx/outFuncs';
 import {snacOpts, xmlOpts} from './lib/snac/opts'
@@ -14,7 +14,7 @@ function App() {
     const funcs = { OpenTag, CloseTag, Text, CDATA, Comment, PI, Attributes, Attribute, Prefix }
     const snac = xml2snac(xmlInput)[0]
 
-    const xml2 = snac2xml([snac], xmlOpts)
+    //const xml2 = snac2xml([snac], xmlOpts)
     const xml3 = xmlOut([snac], [snac], funcs, snacOpts)
 
     return (
@@ -22,12 +22,12 @@ function App() {
             <h2>XML INPUT</h2>
             <pre>{xmlInput}</pre>
             <hr />
-            <h2>SNAC</h2>
+            {/* <h2>SNAC</h2>
             <pre>{JSON.stringify(snac, null, 4)}</pre>
             <hr />
             <h2>SNAC 2 XML</h2>
             <pre>{xml2}</pre>
-            <hr />
+            <hr /> */}
             <h2>XMLOUT</h2>
             <pre>{xml3}</pre>
         </>
